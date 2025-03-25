b0fb696986ef268e381364aee1d9535ec7f91a20
timotheG <gonin.timothe@gmail.com>
2025-03-25 09:53:15 +0100

    🎨💄basic styles + semantic

--- src/components/datepicker/Datepicker.tsx
+++ src/components/datepicker/Datepicker.tsx
@@ -7,9 +7,9 @@ export const Datepicker = ({
 }: DatepickerProps) => {
 
   return (
-    <div>
-      <label htmlFor="">{label}</label>
-      <input type="date" />
+    <div className="flex flex-col gap-2">
+      <label htmlFor={label}>{label}</label>
+      <input type="date" name={label} className="border border-slate-200 rounded py-2 px-3"/>
     </div>
   )
 }
