bc821ad95a2a2c7695fafb20c9b20d8d46c9033a
timotheG <gonin.timothe@gmail.com>
2025-03-28 11:25:15 +0100

    🚚 import a stable configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,15 @@
-import type { StorybookConfig } from '@storybook/react-vite';
-
-const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test",
-    "@storybook/addon-styling-webpack"
+    "@storybook/addon-interactions",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
 };
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,26 @@
-import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
-import { peerDependencies } from "./package.json";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import { peerDependencies } from './package.json'
+import { defineConfig } from 'vite'
 
-export default defineConfig(() => ({
+export default defineConfig({
   plugins: [
     react(),
     dts({
-      exclude: ["**/*.stories.ts", "**/*.test.ts"],
+      insertTypesEntry: true,
+      exclude: ['**/*.stories.ts', '**/*.test.tsx'],
     }),
   ],
   build: {
     lib: {
       entry: './src/index.ts',
-      name: "ui",
-      fileName: (format) =>`ui.${format}.js`,
-      formats: ["es", "cjs", "umd"],
+      name: 'ui',
+      fileName: (format) => `ui.${format}.js`,
+      formats: ['es', 'cjs', 'umd'],
     },
-    rollupOptions: { 
-      external: Object.keys(peerDependencies), 
-      output: { globals: { react: 'React', 'react-dom': 'ReactDOM' } } 
-    }
+    rollupOptions: {
+      external: Object.keys(peerDependencies),
+      output: { globals: { react: 'React', 'react-dom': 'ReactDOM' } },
+    },
   },
-}));
+})
